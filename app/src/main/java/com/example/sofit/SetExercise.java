d14a675be2088b4c5f8152bd9f0002d53365719a
sergio <uo282598@uniovi.es>
2022-10-30 11:21:06 +0100

    Nuevos cambios

--- app/src/main/java/com/example/sofit/SetExercise.java
+++ app/src/main/java/com/example/sofit/SetExercise.java
@@ -46,14 +46,14 @@ public class SetExercise extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(final Menu menu) {
-// Inflate the menu
+
         getMenuInflater().inflate(R.menu.menu_misrutinas, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-//noinspection SimplifiableIfStatement
+
         if (id == R.id.menuItem_misRutinas_misRutinas) {
             startActivity(new Intent(SetExercise.this, RecyclerMyRoutines.class));
         }
