428af7b9552ec46bd44ba6dc962141da6ee03367
diegogz <uo282887@uniovi.es>
2022-11-15 10:59:29 +0100

    Ultimos cambios.

--- app/src/main/java/com/example/sofit/ExercisesOfADay.java
+++ app/src/main/java/com/example/sofit/ExercisesOfADay.java
@@ -1,14 +1,15 @@
 package com.example.sofit;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
-import com.example.sofit.adapters.ListDiasViewAdapter;
 import com.example.sofit.adapters.ListaEjerciciosViewAdapter;
-import com.example.sofit.model.Day;
 
 import java.util.ArrayList;
 
@@ -36,10 +37,33 @@ public class ExercisesOfADay extends AppCompatActivity {
                 new ListaEjerciciosViewAdapter.OnItemClickListener(){
                     @Override
                     public void onItemClick(String item) {
-                        /* Change current routine to the one clicked */
+                        startActivity(new Intent(ExercisesOfADay.this,ExerciseActivity.class));
                     }
                 });
 
         listaExsView.setAdapter(lpAdapter);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+// Inflate the menu
+        getMenuInflater().inflate(R.menu.menu_misrutinas, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+//noinspection SimplifiableIfStatement
+        if (id == R.id.menuItem_misRutinas_misRutinas) {
+            startActivity(new Intent(ExercisesOfADay.this, AddSession.class));
+        }
+        if (id==R.id.menuItem_misRutinas_perfil){
+            startActivity(new Intent(ExercisesOfADay.this, MyProfile.class));
+        }
+        if (id==R.id.menuItem_misRutinas_rutinas){
+            startActivity(new Intent(ExercisesOfADay.this, MyCurrentRoutine.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
